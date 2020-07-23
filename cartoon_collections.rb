3780728eeaf86871c3cae15d2ddea4105e5de3b9
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-23 23:40:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.count do |element|
     element > element.count(4).true?
+  end
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
 end
